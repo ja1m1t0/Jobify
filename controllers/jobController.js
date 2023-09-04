67a8d1c8a6a44b76cdce797ad3daef813242103a
ja1m1t0 <jaime@funed.tech>
2023-09-03 21:53:57 -0600

    up to lesson 77

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,25 +1,11 @@
 import Job from "../models/JobModel.js";
 
-import { nanoid } from "nanoid";
-
-let jobs = [
-  { id: nanoid(), company: "apple", position: "front-end" },
-  { id: nanoid(), company: "google", position: "back-end" },
-];
-
 export const getAllJobs = async (req, res) => {
   res.status(200).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    res.status(400).json({ msg: "Please provide company and position" });
-    return;
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create(req.body);
   res.status(201).json({ job });
 };
 
