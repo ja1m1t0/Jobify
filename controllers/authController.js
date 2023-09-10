c75373c899505d6d84ebc457667b399a9449b995
ja1m1t0 <jaime@funed.tech>
2023-09-09 22:17:53 -0600

    up to lesson 100

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import bcrypt from "bcryptjs";
+import { hashPassword, comparePassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
@@ -15,5 +16,13 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  const user = await User.findOne({ email: req.body.email });
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
+
+  const token = createJWT({ userId: user._id, role: user.role });
+  console.log(token);
+
+  res.json({ token });
 };
