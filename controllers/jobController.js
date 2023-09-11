1436ec7af84cd3da79c5809c79a1f92393e9b11b
ja1m1t0 <jaime@funed.tech>
2023-09-10 22:31:43 -0600

    correct vulnerability in db injection

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,29 +2,33 @@ import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const jobs = await Job.find({ createdBy: req.user.userId.toString() });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId;
-  const job = await Job.create(req.body);
+  req.body.createdBy = req.user.userId.toString();
+  const job = await Job.create(req.body.toString());
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const getSingleJob = async (req, res) => {
-  const job = await Job.findById(req.params.id);
+  const job = await Job.findById(req.params.id.toString());
   res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
-  const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  const updatedJob = await Job.findByIdAndUpdate(
+    req.params.id.toString(),
+    req.body.toString(),
+    {
+      new: true,
+    }
+  );
   res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
-  const removedJob = await Job.findByIdAndDelete(req.params.id);
+  const removedJob = await Job.findByIdAndDelete(req.params.id.toString());
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: removedJob });
 };
